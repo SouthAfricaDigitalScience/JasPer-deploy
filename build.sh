a0141619e28f9fd1d89bd920076e7610d998fd69
Bruce Becker <brucellino@gmail.com>
2015-10-14 18:30:23 +0200

    was using tar instead of unzip

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ else
 fi
 
 # now unpack it into the workspace
-tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+unzip $SRC_DIR/$SOURCE_FILE -d $WORKSPACE
 
 #  generally tarballs will unpack into the NAME-VERSION directory structure. If this is not the case for your application
 #  ie, if it unpacks into a different default directory, either use the relevant tar commands, or change
